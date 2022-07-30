e34cc29c2e1322b1e1a50e8247a71b59d958aa91
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-07-30 16:36:07 +0200

    add app routes

---
+++ lib/src/core/config/routes/app_route.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../../../../main.dart';
+
+class AppRoute {
+  Route generateRoute(RouteSettings route) {
+    switch (route.name) {
+      default:
+        return MaterialPageRoute(builder: (_) => const MyHomePage(title: 'Main',));
+    }
+  }
+}
