a058a152fac01ecb2733aa6650fee4b527dc11ed
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-07-31 22:37:40 +0200

    create dio client

--- lib/injector.dart
+++ lib/injector.dart
@@ -1,8 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:world_news/src/core/usecase/usecase.dart';
-import 'package:world_news/src/core/utils/constant_value.dart';
 import 'package:world_news/src/data/data_sources/remote/api_service.dart';
+import 'package:world_news/src/data/data_sources/remote/dio_clinet.dart';
 import 'package:world_news/src/data/repositories/app_repository_imp.dart';
 import 'package:world_news/src/domain/use_cases/get_data_use_case.dart';
 
@@ -12,23 +12,10 @@ final injector = GetIt.instance;
 
 Future<void> initializeDependencies() async {
 
-  injector.registerLazySingleton<Dio>(() => createDio());
+  injector.registerLazySingleton<Dio>(() => DioClient.initialDio());
   injector.registerLazySingleton<ApiService>(() => ApiService(injector()));
   injector.registerLazySingleton<IAppRepository>(() => AppRepositoryImp(injector()));
   injector.registerLazySingleton<UseCase>(() => GetDataUseCase(injector()));
 
 
-}
-
-Dio createDio(){
-  return Dio(
-    BaseOptions(
-      baseUrl: ConstantValue.kBaseUrl,
-    ),
-  )..interceptors.add(LogInterceptor(
-    error: true,
-    request: true,
-    requestBody: true,
-    responseBody: false,
-  ));
 }
---
+++ lib/src/data/data_sources/remote/dio_clinet.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:world_news/src/core/utils/constant_value.dart';
+
+class DioClient{
+  static late final Dio _dio;
+
+  static Dio initialDio(){
+    _dio = Dio(_baseOptions());
+    if(!kReleaseMode){
+      _dio.interceptors.add(
+        LogInterceptor(
+          error: false,
+          request: true,
+          requestHeader: true,
+          requestBody: true,
+          responseHeader: true,
+          responseBody: false,
+        )
+      );
+    }
+    return _dio;
+  }
+
+  static BaseOptions _baseOptions(){
+    return BaseOptions(
+      baseUrl: ConstantValue.kBaseUrl,
+      connectTimeout: 60 * 1000,
+      receiveTimeout: 60 * 1000,
+      sendTimeout: 60 * 1000,
+      contentType: "application/json",
+    );
+  }
+}
