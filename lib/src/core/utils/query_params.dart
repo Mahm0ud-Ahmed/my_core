f4486c8461f502a89b15bcbf0c44052c8a2e6811
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-07 15:57:30 +0200

    Update query_params.dart

--- lib/src/core/utils/query_params.dart
+++ lib/src/core/utils/query_params.dart
@@ -1,14 +1,36 @@
 import 'package:world_news/src/core/utils/constant_value.dart';
 
 class QueryParams{
-  final String apiKey;
-  final String? queryWord;
+  // final String? apiKey;
   final String endpoint;
+  final String? pathId;
+  final String? queryWord;
+  // final String? sortBy;
+  // final String? language;
+  // final String? sources;
+  // final String? searchIn;
+  // final String? from;
+  // final String? to;
+  // final String? country;
+  // final String? category;
+  // final int? pageSize;
+  // final int? page;
 
   const QueryParams({
     required this.endpoint,
-    this.apiKey = ConstantValue.kApiKey,
+    // this.apiKey = ConstantValue.kApiKey,
+    this.pathId,
     this.queryWord,
+    // this.sortBy,
+    // this.language,
+    // this.sources,
+    // this.searchIn,
+    // this.from,
+    // this.to,
+    // this.country,
+    // this.category,
+    // this.pageSize,
+    // this.page,
   }
 );
 
