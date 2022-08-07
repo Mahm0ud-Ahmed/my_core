c43c651721e76d3adb373bdeb7103485e1de0ad6
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-07 15:54:56 +0200

    add class data state

---
+++ lib/src/core/utils/data_state.dart
@@ -0,0 +1,16 @@
+import 'package:world_news/src/core/error/error_model.dart';
+
+abstract class DataState<T> {
+  final T? data;
+  final ErrorModel? error;
+
+  const DataState({this.data, this.error});
+}
+
+class DataSuccess<T> extends DataState<T> {
+  const DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends DataState<T> {
+  const DataFailed(ErrorModel? error) : super(error: error);
+}
