64c51dcbe1189d34144d37183ede4c4c789ba7c2
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-07 15:56:20 +0200

    execute repo

--- lib/src/data/repositories/app_repository_imp.dart
+++ lib/src/data/repositories/app_repository_imp.dart
@@ -1,38 +1,29 @@
-
-import 'dart:io';
-
-import 'package:dio/dio.dart';
 import 'package:retrofit/dio.dart';
-import 'package:world_news/src/core/error/error_handler.dart';
-import 'package:world_news/src/core/error/error_model.dart';
-import 'package:dartz/dartz.dart';
 import 'package:world_news/src/core/utils/query_params.dart';
 import 'package:world_news/src/data/data_sources/remote/api_service.dart';
-import 'package:world_news/src/domain/entities/article.dart';
 import 'package:world_news/src/domain/repositories/i_app_repository.dart';
 
 class AppRepositoryImp extends IAppRepository{
   final ApiService _apiService;
 
   AppRepositoryImp(this._apiService);
-
+  
   @override
-  Future<Either<ErrorModel, List<Article>>> getEveryThingQuery(QueryParams params) async {
-    final HttpResponse response;
-    try {
-      response = await _apiService.getApiData(
-        apiKey: params.apiKey,
-        querySearch: params.queryWord!,
-        endpoint: params.endpoint,
-      );
-      if(response.response.statusCode == HttpStatus.ok){
-        return Right(response.data.articles);
-      }else{
-        return Left(ErrorHandler.handleError(response.data));
-      }
-
-    } on DioError catch (error) {
-      return Left(ErrorHandler.handleError(error));
-    }
+  Future<HttpResponse> getAllData(QueryParams params) async {
+    return await _apiService.getApiData(
+      // apiKey: params.apiKey,
+      querySearch: params.queryWord,
+      endpoint: params.endpoint,
+    );
+  }
+  
+  @override
+  Future<HttpResponse> getShowDataByPath(QueryParams params) async {
+    return await _apiService.getApiDataByPath(
+      // apiKey: params.apiKey,
+      querySearch: params.queryWord,
+      endpoint: params.endpoint,
+      pathId: params.pathId!,
+    );
   }
 }
--- lib/src/domain/repositories/i_app_repository.dart
+++ lib/src/domain/repositories/i_app_repository.dart
@@ -1,11 +1,10 @@
 
-import 'package:dartz/dartz.dart';
-import 'package:world_news/src/core/error/error_model.dart';
+import 'package:retrofit/retrofit.dart';
 import 'package:world_news/src/core/utils/query_params.dart';
-import 'package:world_news/src/domain/entities/article.dart';
 
 abstract class IAppRepository{
 
-  Future<Either<ErrorModel, List<Article>>> getEveryThingQuery(QueryParams params);
+  Future<HttpResponse> getAllData(QueryParams params);
+  Future<HttpResponse> getShowDataByPath(QueryParams params);
   
 }
