9df64b75e2afa5addaf2ca7833b05a20368b11d2
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-07 15:57:36 +0200

    Update constant_value.dart

--- lib/src/core/utils/constant_value.dart
+++ lib/src/core/utils/constant_value.dart
@@ -1,6 +1,8 @@
 class ConstantValue{
   
-  static const String kBaseUrl = "https://newsapi.org/v2/";
+  // static const String kBaseUrl = "https://newsapi.org/v2/";
+  static const String kBaseUrl = "https://dummyjson.com/";
+  // static const String kBaseUrl = "https://jsonplaceholder.typicode.com/";
   static const String kApiKey = "6faa213056254af2a6383ed811dcd50d";
 
 }
