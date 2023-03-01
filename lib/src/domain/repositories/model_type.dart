653be53a96987f251fcdbf88f39ce019f2494e46
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2023-03-01 22:35:55 +0200

    Update model_type.dart

--- lib/src/domain/repositories/model_type.dart
+++ lib/src/domain/repositories/model_type.dart
@@ -5,22 +5,25 @@ import '../entities/product_model.dart';
 
 class ModelType{
 
-  static T getModel<T>(Object json){
-    Map<String, dynamic> myJson = json as Map<String, dynamic>;
-    switch (T) {
-      case HomeModel:
-        return HomeModel.fromJson(myJson) as T;
-      case ProductModel:
-        return ProductModel.fromJson(myJson) as T;
-      case CourseModel:
-        return CourseModel.fromJson(myJson) as T;
-      case MessageModel:
-        return MessageModel.fromJson(myJson) as T;
-      case String: 
-      case int:
-        return json as T;
-      default:
-        return myJson as T;
+  static T? getModel<T>(Object? json){
+    if(json != null){
+      Map<String, dynamic> myJson = json as Map<String, dynamic>;
+      switch (T) {
+        case HomeModel:
+          return HomeModel.fromJson(myJson) as T;
+        case ProductModel:
+          return ProductModel.fromJson(myJson) as T;
+        case CourseModel:
+          return CourseModel.fromJson(myJson) as T;
+        case MessageModel:
+          return MessageModel.fromJson(myJson) as T;
+        case String: 
+        case int:
+          return json as T;
+        default:
+          return myJson as T;
+      }
     }
+    return null;
   }
 }
