f086b0f435e4cca2426bea69657a35b72cf5599d
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2023-03-02 01:31:46 +0200

    Update app_route.dart

--- lib/src/core/config/routes/app_route.dart
+++ lib/src/core/config/routes/app_route.dart
@@ -1,13 +1,46 @@
+import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:my_core/src/presentation/screens/home/home_screen_page.dart';
 
-import '../../../presentation/screens/home/home_screen_page.dart';
-
-
+import '../injector.dart';
 class AppRoute {
-  static Route generateRoute(RouteSettings route) {
+
+  static GoRouter? _router;
+
+  static GoRouter _generateRoute(){
+    return _router = GoRouter(
+      navigatorKey: injector<GlobalKey<NavigatorState>>(),
+      debugLogDiagnostics: kDebugMode,
+      initialLocation: '/',
+      errorBuilder: (context, state) => SizedBox.shrink(),
+      routes: _routes
+      
+    );
+  }
+
+  GoRouter get route => _router!;
+
+  static final List<RouteBase> _routes = [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreenPage(),
+    ),
+  ];
+
+  AppRoute.init();
+  static AppRoute? _instance;
+  factory AppRoute() {
+    _router = _generateRoute();
+    return _instance ??= AppRoute.init();
+  }
+
+  /* static Route generateRoute(RouteSettings route) {
     switch (route.name) {
       default:
         return MaterialPageRoute(builder: (_) => const HomeScreenPage());
     }
-  }
+  } */
+
+
 }
